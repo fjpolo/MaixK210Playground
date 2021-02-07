4f61d78fa0f881eaf00aa9b11cb25aad511f6fa4
fjpolo <fjpolo@gmail.com>
2021-02-07 18:46:20 +0100

    clean something

--- kendryte_libraries/kendryte_redirect-stdio-standalone/config/ide-hook-main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "fpioa-config.h"
-
-__attribute__((constructor)) void initialize_kendryte_ide_hook()
-{
-    printf("* Kendryte IDE startup before main().\n");
-    ide_config_fpioa();
-}
